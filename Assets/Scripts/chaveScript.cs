371f196b3d4fe421a9f648864d41170bc1c565a9
Caue <119464596+CauePazz@users.noreply.github.com>
2024-11-20 21:53:50 -0300

    Fase Porta e Fase 2

---
+++ Assets/Scripts/chaveScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chaveScript : MonoBehaviour
+{
+    public Transform npc; // O gameObject que o coletável irá seguir
+    public Transform fechadura; // O ponto específico onde o coletável será usado
+    public float vel = 5f; // Velocidade ao seguir
+    public float distMin = 3f; // Distância para "usar" o coletável
+    public GameObject porta;
+    public Sprite portaFechada;
+
+    private bool isSeguindo = false;
+    private bool isEntregue = false;
+    public Collider2D chaveCollider;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Checa se o coletável deve seguir o objeto
+        if (collision.CompareTag("NPC")) // Certifique-se de que o GameObject tem a tag "Player"
+        {
+            npc = collision.transform;
+            isSeguindo = true;
+            Physics2D.IgnoreCollision(collision,chaveCollider, true);
+        }
+    }
+
+    private void Update()
+    {
+        if (isSeguindo && npc != null && !isEntregue)
+        {
+            // Move o coletável em direção ao gameObject
+            transform.position = Vector3.Lerp(transform.position, npc.position, vel * Time.deltaTime);
+        }
+
+        // Checa se chegou no ponto de entrega e está na distância correta
+        if (fechadura != null && Vector3.Distance(transform.position, fechadura.position) < distMin && isSeguindo)
+        {
+            if (Input.GetKeyDown(KeyCode.Q)) // Interação ao pressionar 'E'
+            {
+                UseCollectible();
+            }
+        }
+    }
+
+    private void UseCollectible()
+    {
+        Debug.Log("Coletável usado!");
+        isEntregue = true;
+
+        if (porta != null && portaFechada != null)
+        {
+            SpriteRenderer doorRenderer = porta.GetComponent<SpriteRenderer>();
+            if (doorRenderer != null)
+            {
+                doorRenderer.sprite = portaFechada;
+            }
+        }
+
+        Destroy(gameObject); // Destrói o coletável
+    }
+}
