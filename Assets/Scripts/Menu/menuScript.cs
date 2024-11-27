1a7f4fe4e105062a049c9cdc979978eda1da1428
Caue <119464596+CauePazz@users.noreply.github.com>
2024-11-27 16:02:43 -0300

    Correções

--- Assets/Scripts/Menu/menuScript.cs
+++ Assets/Scripts/Menu/menuScript.cs
@@ -5,9 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class menuScript : MonoBehaviour
 {
+    public GameObject PanelCredits;
     public void Jogar()
     {
         SceneManager.LoadScene("Cutscene");
+    }
+
+    public void Creditos()
+    {
+        PanelCredits.SetActive(!PanelCredits.activeSelf);
     }
 
     public void Sair()
